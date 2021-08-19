4a751955c13b9c7a2ae097784fb85b1ee76185e4
omorihayato <hayato.xxx.8810@gmail.com>
2021-08-19 22:34:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# crypto-transfer-qr
+
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
---
+++ index.js
@@ -0,0 +1,6 @@
+const test = () => {
+  console.log('crypto-transfer-qr initial push');
+}
+
+module.exports = test;
+module.exports.default = test;
