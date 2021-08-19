4d8db2350842570b61f5ee13462f34c13d94fa11
omorihayato <hayato.xxx.8810@gmail.com>
2021-08-20 02:25:49 +0900

    Update

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom'
-import MyComponent from '../../src'
+import CryptoTransferQR from "../../src";
 
-const App = () => <MyComponent/>;
+const App = () => <CryptoTransferQR />;
 render(<App />, document.getElementById('root'));
--- src/components/CryptoTransferQR.js
+++ src/components/CryptoTransferQR.js
@@ -1,7 +1,13 @@
 import React from "react";
 import QRCode from "qrcode.react";
 
-const CryptoTransferQR = ({ contract, chain, to, amount, decimals }) => {
+const CryptoTransferQR = ({
+  contract = "0x6ae7dfc73e0dde2aa99ac063dcf7e8a63265108c",
+  chain = "137",
+  to = "0xF1c51266886c539Bd3e613ff17DBBDF653ae151e",
+  amount = 1000,
+  decimals = "18",
+} = {}) => {
   return (
     <>
       <QRCode
